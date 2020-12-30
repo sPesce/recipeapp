9153f7c54faf2d8e8549b0db4a32c4250a782f27
Steve <stevepesce879@gmail.com>
2020-12-30 15:10:46 -0500

    added index page and controller

---
+++ src/main/java/spesce/springframework/controllers/IndexController.java
@@ -0,0 +1,7 @@
+package spesce.springframework.controllers;
+/** -------------------------------------------------------------
+ * Created By Steve P. on 12/30/2020 
+ * original package: spesce.springframework.controllers 
+ */
+public class IndexController {
+}
